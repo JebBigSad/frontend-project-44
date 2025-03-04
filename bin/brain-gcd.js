a5efe1a45238af7e76f2f56789a210a23849e33c
Klim <klimhonr.ru@gmail.com>
2025-03-04 13:14:40 +0300

    nine

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -4,6 +4,11 @@ import runGame from '../src/index.js';
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const gcd = (a, b) => {
+    if (b === 0) return a;
+    return gcd(b, a % b);
+};
+
 const getQuestionAndAnswer = () => {
     const num1 = getRandomInt(1, 100);
     const num2 = getRandomInt(1, 100);
@@ -14,10 +19,7 @@ const getQuestionAndAnswer = () => {
     return { question, answer };
 };
 
-const gcd = (a, b) => {
-    if (b === 0) return a;
-    return gcd(b, a % b);
-};
+
 
 const gameDescription = 'Find the greatest common divisor of given numbers.';
 runGame(gameDescription, getQuestionAndAnswer);
